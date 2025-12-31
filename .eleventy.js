6a6a30f147e31bf52ef45953cfa9e5e5aa39dc5a
jaamo <jaakko@alajoki.fi>
2025-12-31 10:52:39 +0200

    Added API url correctly.

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,57 @@
-require('dotenv').config();
+require("dotenv").config();
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Copy static assets
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/blog/**/*.jpg");
   eleventyConfig.addPassthroughCopy("src/blog/**/*.png");
-  
+
   // Add global data for environment variables
   eleventyConfig.addGlobalData("env", {
-    guestbookWidgetUrl: process.env.GUESTBOOK_WIDGET_URL || "https://guestbook.tekkipodi.fi/widget.js"
+    guestbookWidgetUrl:
+      process.env.GUESTBOOK_WIDGET_URL ||
+      "https://guestbook.tekkipodi.fi/widget.js",
+    guestbookApiUrl:
+      process.env.GUESTBOOK_API_URL || "https://guestbook.tekkipodi.fi/",
   });
 
   // Add date filter
-  eleventyConfig.addFilter("dateFormat", function(date, format) {
+  eleventyConfig.addFilter("dateFormat", function (date, format) {
     if (!date) return "";
     const d = new Date(date);
     if (isNaN(d.getTime())) return "";
-    
-    const months = ["January", "February", "March", "April", "May", "June", 
-                    "July", "August", "September", "October", "November", "December"];
-    const shortMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", 
-                         "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    
+
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const shortMonths = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
     if (format === "MMMM d, yyyy") {
       return `${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
     } else if (format === "MMM d, yyyy") {
@@ -35,7 +63,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Add collection for blog posts
-  eleventyConfig.addCollection("blog", function(collectionApi) {
+  eleventyConfig.addCollection("blog", function (collectionApi) {
     return collectionApi.getFilteredByGlob("src/blog/*.md").sort((a, b) => {
       return b.date - a.date; // Sort by date, newest first
     });
@@ -46,11 +74,10 @@ module.exports = function(eleventyConfig) {
       input: "src",
       output: "_site",
       includes: "_includes",
-      data: "_data"
+      data: "_data",
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-    templateFormats: ["md", "njk", "html"]
+    templateFormats: ["md", "njk", "html"],
   };
 };
-
